13ac5339f37474d54db1d1e49c43282ec728fdd3
Patrick Klein <131147594+PatrickKL1610@users.noreply.github.com>
2023-05-24 11:47:27 +0200

    Create JS

---
+++ script.js
@@ -0,0 +1,199 @@
+let load = 30;
+let searchedPokemon = [];
+let loadedPokemons = 0;
+
+
+function init() {
+    loadPokemons();
+}
+
+async function loadPokemons() {
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
+    let response = await fetch(url);
+    let responseAsJson = await response.json();
+    let allPokemon = responseAsJson['results'];
+    globalThis.allPokemons = allPokemon;
+    loadCarts();
+}
+
+async function loadCarts() {
+    for (let i = loadedPokemons; i < load; i++) {
+        loadedPokemons++;
+        let thisPokemonUrl = allPokemons[i]['url'];
+        let thisPokemon = await fetch(thisPokemonUrl);
+        let thisCurrentPokemon = await thisPokemon.json();
+        renderCarts(thisCurrentPokemon);
+    }
+}
+
+function renderCarts(thisCurrentPokemon) {
+    document.getElementById('contentContainer').innerHTML += createMiniCarts(thisCurrentPokemon);
+    addTypeBg(thisCurrentPokemon);
+}
+
+function loadMore() {
+    load = load + 20;
+    loadCarts();
+}
+
+function toggleBackBtn() {
+    document.getElementById('btnMore').classList.add('d-none');
+    document.getElementById('btnBack').classList.remove('d-none');
+}
+
+async function selection(i) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    let response = await fetch(url);
+    let currentPokemon = await response.json();
+    globalThis.currentPokemons = currentPokemon;
+    fullscreenView(currentPokemon, i);
+}
+
+async function fullscreenView(currentPokemon, i) {
+    document.getElementById('fullscreenView').classList.remove('d-none');
+    document.getElementById('fullscreenView').innerHTML = renderFullscreen(currentPokemon);
+    fullscreenBg(currentPokemon);
+    renderCardMenu(i);
+    renderArrows(i);
+    document.getElementById('body').style = 'overflow: hidden;';
+}
+
+function closeFullScreen() {
+    document.getElementById('fullscreenView').classList.add('d-none');
+    document.getElementById('body').style = '';
+}
+
+function previous(i) {
+    selection(i - 1);
+}
+function next(i) {
+    selection(i + 1);
+}
+
+
+function renderCardMenu(i) {
+    document.getElementById('fullscreenContainer').innerHTML += fillTable();
+    contentBaseStats();
+}
+
+function renderArrows(i) {
+    document.getElementById('fullscreenContainer').innerHTML += createArrows(i);
+}
+
+function contentAbout() {
+    setIsActiv(1);
+    document.getElementById('cardMenuContend').innerHTML = '';
+    let height = currentPokemons['height'] / 10;
+    let weight = currentPokemons['weight'] / 10;
+
+    document.getElementById('cardMenuContend').innerHTML = createTable(height, weight);
+    fillTableAbility();
+    fillTableItems();
+}
+
+function contentBaseStats() {
+    setIsActiv(2);
+    let pokemonStat = currentPokemons['stats'];
+    document.getElementById('cardMenuContend').innerHTML = '';
+    for (let x = 0; x < pokemonStat.length; x++) {
+        document.getElementById('cardMenuContend').innerHTML += createStatBar(currentPokemons, x);
+    }
+}
+
+function contentMoves() {
+    setIsActiv(3);
+    document.getElementById('cardMenuContend').innerHTML = '';
+    for (let i = 0; i < currentPokemons['moves'].length; i++) {
+        const move = currentPokemons['moves'][i]['move']['name'];
+        document.getElementById('cardMenuContend').innerHTML +=/*html*/`
+            <div class="boxForType">${move}</div>
+        `;
+    }
+}
+
+function fillTableAbility() {
+    for (let i = 0; i < currentPokemons['abilities'].length; i++) {
+        const ability = currentPokemons['abilities'][i]['ability']['name'];
+        document.getElementById('aboutTable').innerHTML += /*html*/`
+        <tr>
+            <td>Ability ${i + 1} : </td><td>${ability}</td>
+        </tr>
+    `;
+    }
+}
+
+function fillTableItems() {
+    for (let i = 0; i < currentPokemons['held_items'].length; i++) {
+        const item = currentPokemons['held_items'][i]['item']['name'];
+        document.getElementById('aboutTable').innerHTML += /*html*/`
+        <tr>
+            <td>Held Item ${i + 1} : </td><td>${item}</td>
+        </tr>
+    `;
+    }
+}
+
+function setIsActiv(isActiv) {
+    if (isActiv == 1) {
+        document.getElementById('menu1').classList.add('isActiv');
+        document.getElementById('menu2').classList.remove('isActiv');
+        document.getElementById('menu3').classList.remove('isActiv');
+    } else if (isActiv == 2) {
+        document.getElementById('menu2').classList.add('isActiv');
+        document.getElementById('menu1').classList.remove('isActiv');
+        document.getElementById('menu3').classList.remove('isActiv');
+    } else {
+        document.getElementById('menu3').classList.add('isActiv');
+        document.getElementById('menu1').classList.remove('isActiv');
+        document.getElementById('menu2').classList.remove('isActiv');
+    }
+}
+
+function getSearchInput() {
+    let search = document.getElementById('searchInput').value;
+    let searchValue = search.toLocaleLowerCase();
+    document.getElementById('searchInput').value = '';
+    filterPokemon(searchValue);
+}
+
+async function filterPokemon(searchValue) {
+    for (let i = 0; i < allPokemons.length; i++) {
+        const pokemon = allPokemons[i];
+        let pokemonName = pokemon['name'];
+        if (pokemonName.includes(searchValue)) {
+            let pokemonResponse = await fetch(pokemon['url']);
+            let pokemonAsJson = await pokemonResponse.json();
+            searchedPokemon.push(pokemonAsJson);
+        }
+    }
+    pokemonSearch();
+}
+
+function pokemonSearch() {
+    let pokemonContainer = document.getElementById('contentContainer');
+    pokemonContainer.innerHTML = '';
+    if (searchedPokemon.length == 0) {
+        pokemonContainer.innerHTML = noPokemonFoundTemplate();
+        toggleBackBtn();
+    } else {
+        document.getElementById('contentContainer').innerHTML = '';
+        for (let i = 0; i < searchedPokemon.length; i++) {
+            let currentPokemons = searchedPokemon[i];
+            renderCarts(currentPokemons, i);
+        }
+        searchedPokemon = [];
+        toggleBackBtn();
+    }
+}
+
+function noPokemonFoundTemplate() {
+    return /*html*/ `
+        <h1>Sorry I could chatch anything</h1>
+    `;
+}
+
+function checkEnter(event) {
+    if (event.keyCode === 13) {
+        getSearchInput();
+    }
+}
---
+++ template.js
@@ -0,0 +1,67 @@
+function createMiniCarts(thisCurrentPokemon) {
+    return /*html*/`
+        <div class="pokemonMiniCard" id="pokemonMiniCard${thisCurrentPokemon['id']}" onclick="selection(${thisCurrentPokemon['id']})">
+            <h3> ${thisCurrentPokemon['name']}</h3>
+            <img src="${thisCurrentPokemon['sprites']['other']['official-artwork']['front_default']}">
+            <div class="types" id="type${thisCurrentPokemon['id']}">
+        </div>
+`
+}
+
+function renderFullscreen(currentPokemon) {
+    return /*html*/`
+        <div class="fullscreenContainer" id="fullscreenContainer" onclick="event.stopPropagation()">
+            <img src="img/close.png" class="closeTag" onclick="closeFullScreen()">
+            <div class="fullscreenBg" id="fullscreenBg">            
+                <h3> ${currentPokemon['name']}</h3>
+                <div class="typeBox" id="typeBox"></div>
+            </div>
+            <div class="fullscreenBgbottom"></div>
+            <img class="pokePic" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}">
+       </div> 
+   `
+}
+
+function createTable(height, weight) {
+    return /*html*/`
+        <table id="aboutTable">
+            <tr>
+                <td>Height:</td><td>${height}m</td>
+            </tr>
+            <tr>
+                <td>Weight:</td><td>${weight}kg</td>
+            </tr>
+        </table>
+    `
+}
+
+function fillTable() {
+    return /*html*/`
+        <div class="cardMenu">
+            <div class="cardMenuText" id="menu1" onclick="contentAbout()">About</div>
+            <div class="cardMenuText" id="menu2" onclick="contentBaseStats()">Base Stats</div>
+            <div class="cardMenuText" id="menu3" onclick="contentMoves()">Moves</div>
+        </div>
+        <div class="cardMenuContend" id="cardMenuContend"></div>
+    `
+}
+
+function createArrows(i) {
+    return /*html*/`
+        <div class="arrowsContainer" id="arrowsContainer">    
+            <div><img class="arrows" src="img/arrow_left.png" onclick="previous(${i})"></div>
+            <div><img class="arrows" src="img/arrow_right.png" onclick="next(${i})"></div>
+        </div>
+    `
+}
+
+function createStatBar(currentPokemons, x) {
+    return /*html*/`
+        <div class="statBar">
+            <div id="statName">${currentPokemons['stats'][x]['stat']['name']}</div>
+            <div class="progress" style="height: 15px;">
+            <div class="progress-bar" role="progressbar" style="width: ${currentPokemons['stats'][x]['base_stat'] - 15}%;" aria-valuenow="" aria-valuemin="0" aria-valuemax="100">${currentPokemons['stats'][x]['base_stat']}</div>
+            </div>
+        </div> 
+    `
+}
